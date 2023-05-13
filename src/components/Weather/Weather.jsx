76d918e8de1dab2e1f1ab18dd1897f92d92ae54d
Ehorov <82938317+Ehorov@users.noreply.github.com>
2023-05-13 04:15:43 +0300

    change weather api

--- src/components/Weather/Weather.jsx
+++ src/components/Weather/Weather.jsx
@@ -1,4 +1,8 @@
 import React, { useState, useEffect } from "react";
+import Rain from "../Rain/Rain";
+import Snowfall from "../Snow/Snow";
+import Stars from "../Stars/Stars";
+import s from "./Weather.module.css";
 
 const Weather = (props) => {
   const { date, startTime } = props;
@@ -8,21 +12,25 @@ const Weather = (props) => {
 
   useEffect(() => {
     if (lastDateTime.date !== date || lastDateTime.startTime !== startTime) {
-      const timestamp = new Date(`${date} ${startTime}`).getTime() / 1000;
-      const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Dnipro&appid=${process.env.REACT_APP_OPEN_WEATHER_MAP_KEY}&units=metric&dt=${timestamp}`;
+      const apiUrl = `https://api.weatherbit.io/v2.0/forecast/daily?city=Dnipro&key=9bc67e2b86874c51ac795b99781460ed`;
 
       fetch(apiUrl)
         .then((response) => {
           if (!response.ok) {
-            throw new Error("Failed to fetch weather data from OpenWeatherMap");
+            throw new Error("Failed to fetch weather data from Weatherbit.io");
           }
-
           return response.json();
         })
         .then((data) => {
+          const weatherData = data.data.find(
+            (item) => item.valid_date === date
+          );
+          if (!weatherData) {
+            throw new Error(`No weather data available for the date ${date}`);
+          }
           setWeather({
-            temperature: Math.round(data.main.feels_like),
-            description: data.weather[0].description,
+            temperature: Math.round(weatherData.app_max_temp),
+            description: weatherData.weather.description,
           });
         })
         .catch((error) => {
@@ -31,7 +39,7 @@ const Weather = (props) => {
 
       setLastDateTime({ date, startTime });
     }
-  }, [date, startTime]);
+  }, [date, startTime, lastDateTime]);
 
   if (error) {
     return <div>{error}</div>;
@@ -40,14 +48,66 @@ const Weather = (props) => {
   if (!weather) {
     return <div>Loading...</div>;
   }
-  console.log(weather.temperature);
-  console.log(weather.description);
-  console.log(date, startTime);
 
   return (
     <div>
       <p>Temperature: {weather.temperature}°C</p>
       <p>Description: {weather.description}</p>
+      {weather.description.includes("clouds") ||
+      weather.description.toLowerCase().includes("rain") ||
+      weather.description.toLowerCase().includes("drizzle") ||
+      weather.description.includes("Thunderstorm") ||
+      weather.description.toLowerCase().includes("snow") ? (
+        <div className={s.clouds}>
+          <img
+            src={process.env.PUBLIC_URL + "/weather/clouds/cloud1.png"}
+            style={{ "--i": 1 }}
+            alt="cloud1"
+          />
+          <img
+            src={process.env.PUBLIC_URL + "/weather/clouds/cloud2.png"}
+            style={{ "--i": 2 }}
+            alt="cloud2"
+          />
+          <img
+            src={process.env.PUBLIC_URL + "/weather/clouds/cloud3.png"}
+            style={{ "--i": 3 }}
+            alt="cloud3"
+          />
+          <img
+            src={process.env.PUBLIC_URL + "/weather/clouds/cloud4.png"}
+            style={{ "--i": 4 }}
+            alt="cloud4"
+          />
+          <img
+            src={process.env.PUBLIC_URL + "/weather/clouds/cloud5.png"}
+            style={{ "--i": 5 }}
+            alt="cloud5"
+          />
+        </div>
+      ) : null}
+
+      {weather.description === "clear sky" ||
+      (weather.description.includes("clouds") &&
+        weather.description !== "Overcast clouds") ? (
+        <div className={s.sun}></div>
+      ) : null}
+
+      {weather.description === "clear sky" ||
+      (weather.description.includes("clouds") &&
+        weather.description !== "Overcast clouds") ? (
+        <div className={s.moon}>
+          <img src={process.env.PUBLIC_URL + "/weather/moon.svg"} alt="moon" />
+        </div>
+      ) : null}
+      {weather.description === "clear sky" ? <Stars /> : null}
+
+      {weather.description.toLowerCase().includes("rain") ||
+      weather.description.toLowerCase().includes("drizzle") ||
+      weather.description.includes("Thunderstorm") ? (
+        <Rain />
+      ) : null}
+      {weather.description.toLowerCase().includes("snow") ? <Snowfall /> : null}
     </div>
   );
 };
